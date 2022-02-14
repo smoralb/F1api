ee1ac1a97cb6bcb80051f46d2734e294613f2780
smoralber@gmail.com <smoralber@gmail.com>
2022-02-14 16:06:42 +0100

    Add dotenv to store app secrets

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-const DEFAULT_PORT = 3000
-const connectionString = 'mongodb+srv://smoralber:admin@cluster0.bodor.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const DEFAULT_PORT = 3000;
 
 // require function is a Node js function that helps to use this library, it is like and import
-const express = require('express')
-const bodyParser= require('body-parser')
-const mongoClient = require('mongodb').MongoClient
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoClient = require("mongodb").MongoClient;
 
 //Creates an express application
-const app = express()
-/*
-The urlencoded method within body-parser tells body-parser to extract data from 
-the <form> element and add them to the body property in the request object.
-
-That's because Express does not read data from the form.
-*/
-app.use(bodyParser.urlencoded( { extended: true } ))
+const app = express();
 
 //Mongo DB supports promises and callbacks
 
@@ -26,23 +19,34 @@ mongoClient.connect(connectionString, (err, client) => {
   })
   */
 
-  //And this, with promises
-  mongoClient.connect(connectionString, { useUnifiedTopology: true })
-  .then(client => {
-      console.log('Connected to DB')
-  }).catch ( error => console.log(error))
+//And this, with promises
+mongoClient
+  .connect(process.env.DATABASE_STRING_SECRET, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected to DB");
+    const db = client.db("F1-api");
+    /*
+    The urlencoded method within body-parser tells body-parser to extract data from 
+    the <form> element and add them to the body property in the request object.
+    
+    That's because Express does not read data from the form.
+    */
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-//ROUTES
-app.get('/', (req, res) => {
-    res.send("Hello World again")
-})
+    //ROUTES
 
-app.get('/index', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+    app.get("/", (req, res) => {
+      res.send("Hello World again");
+    });
 
-app.post('/teams', (req, res) => {
-    console.log(req.body);
-})
+    app.get("/index", (req, res) => {
+      res.sendFile(__dirname + "/index.html");
+    });
 
-app.listen(DEFAULT_PORT, () => console.log('Server started'))
+    app.post("/teams", (req, res) => {
+      console.log(req.body);
+    });
+
+    app.listen(DEFAULT_PORT, () => console.log("Server started"));
+  })
+  .catch((error) => console.log(error));
