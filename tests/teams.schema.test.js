96d8bfa311eaf07c0cd3e05fe5c1a1e22d681251
smoralber@gmail.com <smoralber@gmail.com>
2022-02-24 19:08:05 +0100

    Tests for schemas

--- tests/teams.schema.test.js
+++ tests/teams.schema.test.js
@@ -29,12 +29,23 @@ describe("Teams schema", () => {
   it("should be invalid if color is empty", (done) => {
     var team = new Team({ color: "" });
 
-    team.validate(function (err) {
+    team.validate((err) => {
       console.log(team);
       console.log(err.message);
       assert.isNotEmpty(err.message);
       assert.notExists(team.name);
       assert.empty(team.color);
+      done();
+    });
+  });
+  it("should be a valid Team", (done) => {
+    var team = new Team({ name: "Team test name", color: "Team color test" });
+
+    team.validate((err) => {
+      console.log(team);
+      assert.isNotEmpty(team.name);
+      assert.isNotEmpty(team.color);
+      assert.notExists(err);
       done();
     });
   });
