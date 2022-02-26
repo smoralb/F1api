d2bffcc03a7d4deb9e04427593ff30ef29f66640
smoralber@gmail.com <smoralber@gmail.com>
2022-02-26 14:52:40 +0100

    Add put and post methods

--- tests/teams.controllers.test.js
+++ tests/teams.controllers.test.js
@@ -1,70 +1,85 @@
 let chai = require("chai");
 let chaiHttp = require("chai-http");
 const assert = chai.assert;
-const should = chai.should;
-const have = chai.have;
 const app = require("../app");
 const Team = require("../src/models/teams");
 
 chai.use(chaiHttp);
 
-describe("GET teams", () => {
-  beforeEach((done) => {
-    Team.deleteMany({}, (err) => {
-      done();
-    });
+beforeEach((done) => {
+  Team.deleteMany({}, (err) => {
+    done();
   });
+});
+
+describe("GET teams", () => {
   it("with no teams added", async () => {
-    const result = await chai.request(app).get("/teams");
-    assert.equal(result.statusCode, 200);
-    assert.empty(result.body);
+    await chai
+      .request(app)
+      .get("/teams")
+      .then((result) => {
+        assert.equal(result.statusCode, 200);
+        console.log(result.body);
+        assert.isEmpty(result.body);
+      })
+      .catch((err) => console.error(err));
   });
   it("with teams added", () => {
-    let team = new Team({ name: "Mercedes", color: "Blue" });
+    let team = new Team({ name: "Test", color: "Test" });
     team.save(async (err, team) => {
       await chai
         .request(app)
         .get("/teams")
         .send(team)
-        .end((err, res) => {
-          assert.equal(res.statusCode, 200);
-          assert.isNotEmpty(res.body);
-        });
+        .then((result) => {
+          assert.equal(result.statusCode, 200);
+          console.log(result.body);
+        })
+        .catch((err) => console.error(err));
     });
   });
   it("get team by id", () => {
-    let team = new Team({ name: "Test", color: "test" });
+    let team = new Team({ name: "Test", color: "Test" });
     team.save(async (err, team) => {
       await chai
         .request(app)
         .get("/teams/" + team.id)
         .send(team)
-        .end((err, res) => {
+        .then((res) => {
           assert.equal(res.statusCode, 200);
-          assert.isNotEmpty(res.body);
         });
     });
   });
 });
 
 describe("POST teams", () => {
-  beforeEach((done) => {
-    Team.deleteMany({}, (err) => {
-      done();
-    });
-  });
-  it("send team in the request body", (done) => {
-    chai
+  it("send team in the request body", async () => {
+    let team = new Team({ name: "Test", color: "Test" });
+    await chai
       .request(app)
       .post("/teams/addTeam")
-      .send({ name: "Test", color: "Test" })
+      .send(team)
       .end((err, res) => {
         assert.equal(res.statusCode, 200);
-        assert.isNotEmpty(res.body);
-        assert.equal(res.body.name, "Test");
-        assert.equal(res.body.color, "Test");
-        assert.isNotEmpty(res.body);
-        done();
       });
+  });
+});
+
+describe("PUT teams", () => {
+  it("update by id", async () => {
+    let team = new Team({ name: "Test", color: "Test" });
+    let teamUpdated = new Team({ name: "Test 2", color: "Test 2" });
+    await team.save((err, team) => {
+      chai
+        .request(app)
+        .put("/teams/update/" + team.id)
+        .set("content-type", "application/json")
+        .send(teamUpdated)
+        .end((err, res) => {
+          assert.equal(res.statusCode, 200);
+          assert.equal(res.body.name, teamUpdated.name);
+          assert.equal(res.body.color, teamUpdated.color);
+        });
+    });
   });
 });
